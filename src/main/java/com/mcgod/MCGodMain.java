8c48cc42de66e039b227ed7288461a0d5476ff3d
Vitaly Vakhteev <vitaly.v@techusage.com>
2024-05-14 12:54:49 -0700

    Finish first version

--- src/main/java/com/mcgod/MCGodMain.java
+++ src/main/java/com/mcgod/MCGodMain.java
@@ -6,8 +6,11 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,19 +23,34 @@ public class MCGodMain extends JavaPlugin {
     private Random random;
     private double baseChance = 0.2; // Base chance of wishes
     private int intervalTaskId;
+    private boolean isHolySiteValid;
+    private String apiKey;
 
     @Override
     public void onEnable() {
         // Save the default config
         this.saveDefaultConfig();
         FileConfiguration config = this.getConfig();
-        holySiteLocation = new Location(
-                Bukkit.getWorld(config.getString("holy-site.world")),
-                config.getDouble("holy-site.x"),
-                config.getDouble("holy-site.y"),
-                config.getDouble("holy-site.z")
-        );
+
+        String worldName = config.getString("holy-site.world");
+        double x = config.getDouble("holy-site.x", 0);
+        double y = config.getDouble("holy-site.y", 0);
+        double z = config.getDouble("holy-site.z", 0);
+
+        if (worldName != null && Bukkit.getWorld(worldName) != null) {
+            holySiteLocation = new Location(Bukkit.getWorld(worldName), x, y, z);
+            isHolySiteValid = true;
+        } else {
+            getLogger().severe("Invalid holy site configuration! Sacrifice command will be disabled.");
+            isHolySiteValid = false;
+        }
+
         actionInterval = config.getInt("action-interval", 6000); // Default to 5 minutes if not set
+        apiKey = config.getString("openai.apiKey");
+        if (apiKey == null || apiKey.isEmpty()) {
+            getLogger().severe("OpenAI API key not found in config.yml!");
+        }
+
         playerWishes = new HashMap<>();
         random = new Random();
 
@@ -50,7 +68,7 @@ public class MCGodMain extends JavaPlugin {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
             if (command.getName().equalsIgnoreCase("pray")) {
@@ -63,6 +81,10 @@ public class MCGodMain extends JavaPlugin {
                 }
                 return true;
             } else if (command.getName().equalsIgnoreCase("sacrifice")) {
+                if (!isHolySiteValid) {
+                    player.sendMessage("Sacrifices are currently disabled due to an invalid holy site configuration.");
+                    return true;
+                }
                 if (player.getLocation().distance(holySiteLocation) < 5) {
                     if (player.getInventory().getItemInMainHand().getType() != Material.AIR) {
                         player.getInventory().setItemInMainHand(null); // Remove the item
@@ -90,40 +112,204 @@ public class MCGodMain extends JavaPlugin {
                 Player player = entry.getKey();
                 String wish = entry.getValue();
                 if (random.nextDouble() < baseChance) {
-                    String modification = getClosestModification(wish);
-                    executeWorldModification(modification);
-                    player.sendMessage("ChatGPT has granted your wish: " + modification);
+                    new BukkitRunnable() {
+                        @Override
+                        public void run() {
+                            String modification = generateCommandForWish(wish);
+                            if (modification != null) {
+                                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), modification);
+                                player.sendMessage("ChatGPT has granted your wish: " + modification);
+                            } else {
+                                modification = getRandomModification();
+                                executeWorldModification(modification);
+                                player.sendMessage("ChatGPT has granted a wish: " + modification);
+                            }
+                        }
+                    }.runTaskAsynchronously(MCGodMain.this);
                 } else {
                     player.sendMessage("ChatGPT has ignored your wish.");
                 }
             }
-            playerWishes.clear(); // Clear wishes for the next interval
+            playerWishes.clear(); // Clear wishes
         }
     }
 
     private String getRandomModification() {
-        // Todo: Return a random world modification
-        String[] modifications = {"remove_water", "set_fire", "give_item"}; // Dumb but will do for now
+        // Basic actions if ChatGPT is inaccessible
+        String[] modifications = {"set_fire", "build_cabin", "spawn_animals", "give_item", "smite_player", "teleport_player"};
         return modifications[random.nextInt(modifications.length)];
     }
 
-    private String getClosestModification(String wish) {
-        // Todo: Return the closest modification based on the wish
-        return getRandomModification();
-    }
-
     private void executeWorldModification(String modification) {
-        // Todo: Add world modifications, think of more mods to add
         switch (modification) {
-            case "remove_water":
-                break;
             case "set_fire":
+                setFire();
+                break;
+            case "build_cabin":
+                buildCabin();
+                break;
+            case "spawn_animals":
+                spawnAnimals();
                 break;
             case "give_item":
+                giveItem();
+                break;
+            case "smite_player":
+                smitePlayer();
+                break;
+            case "teleport_player":
+                teleportPlayer();
                 break;
             default:
                 break;
         }
         Bukkit.broadcastMessage("ChatGPT is performing an action: " + modification);
+    }
+
+    private void setFire() {
+        if (random.nextBoolean()) {
+            // Set a radius of 3 blocks around a random player on fire
+            Player player = getRandomPlayer();
+            if (player != null) {
+                Location loc = player.getLocation();
+                int radius = 3;
+                for (int x = -radius; x <= radius; x++) {
+                    for (int z = -radius; z <= radius; z++) {
+                        Location fireLoc = loc.clone().add(x, 0, z);
+                        fireLoc.getBlock().setType(Material.FIRE);
+                    }
+                }
+            }
+        } else {
+            // Set fire to the nearest village from a random player
+            Player player = getRandomPlayer();
+            if (player != null) {
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        String locateCommand = "execute in " + player.getWorld().getName() + " run locate structure minecraft:village";
+                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), locateCommand);
+                        Location villageLocation = parseLocateResult();
+                        if (villageLocation != null) {
+                            setVillageOnFire(villageLocation);
+                        }
+                    }
+                }.runTaskAsynchronously(this);
+            }
+        }
+    }
+
+    private void setVillageOnFire(Location villageLocation) {
+        int radius = 30;
+        for (int x = -radius; x <= radius; x++) {
+            for (int y = -radius; y <= radius; y++) {
+                for (int z = -radius; z <= radius; z++) {
+                    Location blockLoc = villageLocation.clone().add(x, y, z);
+                    if (blockLoc.getBlock().getType() == Material.OAK_PLANKS) {
+                        blockLoc.getBlock().setType(Material.FIRE);
+                    }
+                }
+            }
+        }
+    }
+
+    private Location parseLocateResult() {
+        // This method should parse the result of the locate command and return the village location
+        // For demonstration purposes, we'll return a fixed location
+        // Replace this with actual parsing logic
+        return new Location(Bukkit.getWorlds().get(0), 100, 64, 100);
+    }
+
+    private void buildCabin() {
+        Player player = getRandomPlayer();
+        if (player != null) {
+            Location loc = player.getLocation();
+            int width = 5, height = 5, depth = 5;
+            loc = loc.add(random.nextInt(20) - 10, 0, random.nextInt(20) - 10); // Random location around player
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    for (int z = 0; z < depth; z++) {
+                        Location blockLoc = loc.clone().add(x, y, z);
+                        if (y == 0 || y == height - 1 || x == 0 || x == width - 1 || z == 0 || z == depth - 1) {
+                            blockLoc.getBlock().setType(Material.OAK_WOOD);
+                        } else {
+                            blockLoc.getBlock().setType(Material.AIR);
+                        }
+                    }
+                }
+            }
+            Location doorLoc = loc.clone().add(1, 1, 0);
+            doorLoc.getBlock().setType(Material.OAK_DOOR);
+        }
+    }
+
+    private void spawnAnimals() {
+        Player player = getRandomPlayer();
+        if (player != null) {
+            EntityType[] animals = {EntityType.PIG, EntityType.CHICKEN, EntityType.COW, EntityType.SHEEP, EntityType.WOLF, EntityType.CAT};
+            EntityType animal = animals[random.nextInt(animals.length)];
+            for (int i = 0; i < 5; i++) {
+                player.getWorld().spawnEntity(player.getLocation(), animal);
+            }
+        }
+    }
+
+    private void giveItem() {
+        Player player = getRandomPlayer();
+        if (player != null) {
+            Material[] items = {Material.IRON_SWORD, Material.DIAMOND_SWORD, Material.IRON_PICKAXE, Material.DIAMOND_PICKAXE, Material.IRON_HELMET, Material.DIAMOND_HELMET, Material.GOLD_INGOT, Material.IRON_INGOT, Material.BRICK};
+            Material item = items[random.nextInt(items.length)];
+            player.getInventory().addItem(new org.bukkit.inventory.ItemStack(item, 1));
+        }
+    }
+
+    private void smitePlayer() {
+        Player player = getRandomPlayer();
+        if (player != null) {
+            player.getWorld().strikeLightning(player.getLocation());
+        }
+    }
+
+    private void teleportPlayer() {
+        Player player1 = getRandomPlayer();
+        Player player2 = getRandomPlayer();
+        if (player1 != null && player2 != null && !player1.equals(player2)) {
+            player1.teleport(player2.getLocation());
+        }
+    }
+
+    private Player getRandomPlayer() {
+        Player[] players = Bukkit.getOnlinePlayers().toArray(new Player[0]);
+        if (players.length > 0) {
+            return players[random.nextInt(players.length)];
+        }
+        return null;
+    }
+
+    private Player[] getPlayers() {
+        return Bukkit.getOnlinePlayers().toArray(new Player[0]);
+    }
+
+    private String generateCommandForWish(String wish) {
+        if (apiKey == null || apiKey.isEmpty()) {
+            getLogger().severe("OpenAI API key not found in config.yml!");
+            return null;
+        }
+
+        try {
+            // Call OpenAI API to generate the command
+            OpenAIClient openAIClient = new OpenAIClient(apiKey);
+            Player[] players = getPlayers();
+            String response = openAIClient.generateCommand(wish, players);
+            return response != null ? response : defaultCommand();
+        } catch (Exception e) {
+            getLogger().severe("Error generating command from OpenAI: " + e.getMessage());
+            return null;
+        }
+    }
+
+    private String defaultCommand() {
+        executeWorldModification(getRandomModification());
+        return null;
     }
 }
---
+++ src/main/java/com/mcgod/OpenAIClient.java
@@ -0,0 +1,66 @@
+package com.mcgod;
+
+import org.bukkit.entity.Player;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import okhttp3.*;
+
+import java.io.IOException;
+
+public class OpenAIClient {
+    private static final String OPENAI_API_URL = "https://api.openai.com/v1/chat/completions";
+    private final String apiKey;
+    private final Gson gson;
+
+    public OpenAIClient(String apiKey) {
+        this.apiKey = apiKey;
+        this.gson = new Gson();
+    }
+
+    public String generateCommand(String wish, Player[] players) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        JsonObject json = new JsonObject();
+        json.addProperty("model", "gpt-3.5-turbo-0125");
+        JsonArray messages = new JsonArray();
+
+        JsonObject systemMessage = new JsonObject();
+        systemMessage.addProperty("role", "system");
+        systemMessage.addProperty("content", "You are a Minecraft plugin that generates commands based on players' wishes.");
+        messages.add(systemMessage);
+
+        JsonObject userMessage = new JsonObject();
+        StringBuilder playersNameAndLoc = new StringBuilder();
+        for (Player player : players) {
+            playersNameAndLoc.append(player.getName()).append(" at ").append(player.getLocation().getBlockX()).append(", ").append(player.getLocation().getBlockY()).append(", ").append(player.getLocation().getBlockZ()).append("; ");
+        }
+
+        userMessage.addProperty("role", "user");
+        userMessage.addProperty("content", "Generate a Minecraft command to fulfill the wish: \"" + wish + "\". Players: " + playersNameAndLoc);
+        messages.add(userMessage);
+
+        json.add("messages", messages);
+
+        RequestBody body = RequestBody.create(json.toString(), MediaType.get("application/json; charset=utf-8"));
+        Request request = new Request.Builder()
+                .url(OPENAI_API_URL)
+                .addHeader("Authorization", "Bearer " + apiKey)
+                .post(body)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            assert response.body() != null;
+            JsonObject responseBody = gson.fromJson(response.body().string(), JsonObject.class);
+            JsonArray choices = responseBody.getAsJsonArray("choices");
+            if (!choices.isEmpty()) {
+                JsonObject firstChoice = choices.get(0).getAsJsonObject();
+                JsonObject messageContent = firstChoice.getAsJsonObject("message");
+                return messageContent.get("content").getAsString().trim();
+            }
+        }
+        return null;
+    }
+}
